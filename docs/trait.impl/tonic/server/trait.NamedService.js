9edbf8e950284a744f9b9e2bed119f9f58e9a46b
qzhuyan <qzhuyan@users.noreply.github.com>
2025-10-23 14:01:31 +0000

    deploy: 97d7ba4ad9bd1c1bc4c09953e75ad4a2428232c3

--- docs/trait.impl/tonic/server/trait.NamedService.js
+++ docs/trait.impl/tonic/server/trait.NamedService.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["mqtt_grpc_proxy",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://docs.rs/tonic/0.12.3/tonic/server/trait.NamedService.html\" title=\"trait tonic::server::NamedService\">NamedService</a> for <a class=\"struct\" href=\"mqtt_grpc_proxy/mqtt_unified_pb/mqtt_relay_service_server/struct.MqttRelayServiceServer.html\" title=\"struct mqtt_grpc_proxy::mqtt_unified_pb::mqtt_relay_service_server::MqttRelayServiceServer\">MqttRelayServiceServer</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["mqtt_grpc_proxy",[["impl&lt;T&gt; NamedService for <a class=\"struct\" href=\"mqtt_grpc_proxy/mqtt_unified_pb/mqtt_relay_service_server/struct.MqttRelayServiceServer.html\" title=\"struct mqtt_grpc_proxy::mqtt_unified_pb::mqtt_relay_service_server::MqttRelayServiceServer\">MqttRelayServiceServer</a>&lt;T&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[457]}
+//{"start":57,"fragment_lengths":[315]}
