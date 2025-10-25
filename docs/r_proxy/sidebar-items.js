3cc64e56891bf339fda5a774bb87f6890f569a51
qzhuyan <qzhuyan@users.noreply.github.com>
2025-10-25 15:33:58 +0000

    deploy: e331c197f192b6934b42ecce6d6253904e20587d

--- docs/r_proxy/sidebar-items.js
+++ docs/r_proxy/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["handle_new_incoming_tcp","main","run_proxy","setup_grpc_connection","start_streaming_loop","wait_for_mqtt_connect"],"struct":["RProxyState","StreamingClientConnection"]};
+window.SIDEBAR_ITEMS = {"fn":["handle_new_incoming_tcp","main","run_proxy","setup_grpc_streaming","start_streaming_loop","wait_for_mqtt_connect"],"struct":["RProxyState","StreamingClientConnection"]};
--- docs/search.desc/r_proxy/r_proxy-desc-0-.js
+++ docs/search.desc/r_proxy/r_proxy-desc-0-.js
@@ -1,1 +1,1 @@
-searchState.loadedDescShard("r_proxy", 0, "Returns the argument unchanged.\nReturns the argument unchanged.\nCalls <code>U::from(self)</code>.\nCalls <code>U::from(self)</code>.")
+searchState.loadedDescShard("r_proxy", 0, "Returns the argument unchanged.\nReturns the argument unchanged.\nCalls <code>U::from(self)</code>.\nCalls <code>U::from(self)</code>.\nSetup a new gRPC bidirectional stream using the shared …")
