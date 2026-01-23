48e951d3b6161c2bfef7122d1ecb3848edc94ec1
qzhuyan <qzhuyan@users.noreply.github.com>
2026-01-23 08:25:18 +0000

    deploy: a5a4bf8019776a0589da0436245107d51113e42b

--- docs/trait.impl/core/ops/drop/trait.Drop.js
+++ docs/trait.impl/core/ops/drop/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["flowsdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"flowsdk/mqtt_client/async_client/struct.AsyncMqttClient.html\" title=\"struct flowsdk::mqtt_client::async_client::AsyncMqttClient\">AsyncMqttClient</a>"]]]]);
+    var implementors = Object.fromEntries([["flowsdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"flowsdk/mqtt_client/async_client/struct.AsyncMqttClient.html\" title=\"struct flowsdk::mqtt_client::async_client::AsyncMqttClient\">AsyncMqttClient</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
