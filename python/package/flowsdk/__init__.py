cae326b8baf9ec6cb7466fb35a3b27e937992dae
William Yang <mscame@gmail.com>
2026-02-02 10:41:57 +0100

    Add reusable FlowMqttClient async module

--- python/package/flowsdk/__init__.py
+++ python/package/flowsdk/__init__.py
@@ -2,3 +2,10 @@ try:
     from .flowsdk_ffi import *
 except ImportError:
     pass
+
+try:
+    from .async_client import FlowMqttClient, FlowMqttProtocol
+except ImportError:
+    pass
+
+__all__ = ['FlowMqttClient', 'FlowMqttProtocol', 'MqttEngineFfi', 'TlsMqttEngineFfi', 'QuicMqttEngineFfi', 'MqttOptionsFfi']
