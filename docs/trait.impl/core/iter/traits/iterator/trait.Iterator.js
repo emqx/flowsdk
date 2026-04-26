10b74b2b616e12feda2eed5a925232dd8546700b
qzhuyan <qzhuyan@users.noreply.github.com>
2026-04-26 12:52:52 +0000

    deploy: 9143601a95e4a6aae5f220326f06e093f831b772

--- docs/trait.impl/core/iter/traits/iterator/trait.Iterator.js
+++ docs/trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["flowsdk",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"flowsdk/mqtt_serde/parser/stream/struct.MqttStream.html\" title=\"struct flowsdk::mqtt_serde::parser::stream::MqttStream\">MqttStream</a>&lt;T&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a>,</div>",0]]]]);
+    const implementors = Object.fromEntries([["flowsdk",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"flowsdk/mqtt_serde/parser/stream/struct.MqttStream.html\" title=\"struct flowsdk::mqtt_serde::parser::stream::MqttStream\">MqttStream</a>&lt;T&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a>,</div>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
